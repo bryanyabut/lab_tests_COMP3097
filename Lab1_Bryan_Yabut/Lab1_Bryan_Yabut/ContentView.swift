4fe64cc2457963eae1c2f09361e77fcf831b0856
bryanyabut <bryan.yabut@outlook.com>
2026-02-28 16:49:49 -0500

    added zstack

--- Lab1_Bryan_Yabut/Lab1_Bryan_Yabut/ContentView.swift
+++ Lab1_Bryan_Yabut/Lab1_Bryan_Yabut/ContentView.swift
@@ -29,11 +29,9 @@ struct ContentView: View {
 
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.white.edgesIgnoringSafeArea(.all)
+            
         }
         .padding()
     }
