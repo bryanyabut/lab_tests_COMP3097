391159747357aea6c8bb18788a68978ae3722df3
bryanyabut <bryan.yabut@outlook.com>
2026-02-28 16:37:35 -0500

    first commit

---
+++ Lab1_Bryan_Yabut/Lab1_Bryan_Yabut/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab1_Bryan_Yabut
+//
+//  Created by Bryan Yabut on 2026-02-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab1_Bryan_Yabut/Lab1_Bryan_Yabut/Lab1_Bryan_YabutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Bryan_YabutApp.swift
+//  Lab1_Bryan_Yabut
+//
+//  Created by Bryan Yabut on 2026-02-28.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Bryan_YabutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
